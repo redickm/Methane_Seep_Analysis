9a0449a8676a39b5bb502904079bd5a1ee20b629
redickm <119533877+redickm@users.noreply.github.com>
2023-09-22 08:12:39 -0700

    Update combine_CanopusSirius5_quant.sh

--- combine_CanopusSirius5_quant.sh
+++ combine_CanopusSirius5_quant.sh
@@ -4,4 +4,4 @@
 #arg3: name for combined output
 #arg4: Feature list ID (no spaces, can be a date, file name etc. output will be arg4_IDsExtracted.tsv and will include the canopus summary with an additional column for feature IDs. You can leave it as Canopus and have it get overwritten each time if you want.)
 #both input files (args 1 & 2) must be generated from same mzmine feature list
-./combine_CanopusSirius5_quant.py 062922_mzmine3_seep_quant.tsv canopus_compound_summary.tsv 20220629_5m_mat_canopus_combined.txt 20220629_canopus
+./combine_CanopusSirius5_quant.py 062922_mzmine3_seep_quant.tsv canopus_compound_summary.tsv canopus_peakarea_combined.txt 20220629_canopus
